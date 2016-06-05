6222f94ae2333faf12b70ccf20fcb95ce377f7fa
Rick Song <ricksong@squareup.com>
2016-06-05 03:21:54 -0700

    Fix up the param parsing

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -14,12 +14,13 @@ defmodule FdlMessengerBot.PageController do
 
   def webhook(conn, %{"entry" => entry}) do
     page_access_token = System.get_env("FACEBOOK_PAGE_ACCESS_TOKEN")
-    IO.inspect(entry)
-    messaging = hd(entry)["messaging"]
-    message = hd(messaging)["message"]["text"]
-    # user_id = messaging["sender"]["id"]
-    IO.inspect(page_access_token)
-    IO.inspect(message)
+
+    messaging = hd(hd(entry)["messaging"])
+    message = messaging["message"]["text"]
+    user_id = messaging["sender"]["id"]
+
+    IO.inspect("sender: #{user_id} - #{message}")
+
     # MessengerAPI.start
     # FdlMessengerBot.MessengerAPI.start
     # FdlMessengerBot.MessengerAPI.postMessage(%{:user_id => user_id, :message => message})
