2be7809b57329d89a33caa0b4206945b097d7afe
Bowen Sun <ukisami@gmail.com>
2016-06-05 03:05:07 -0700

    Update API

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -15,12 +15,13 @@ defmodule FdlMessengerBot.PageController do
   def webhook(conn, %{"entry" => entry}) do
     page_access_token = System.get_env("FACEBOOK_PAGE_ACCESS_TOKEN")
     messaging = hd(entry)["messaging"]
-    user_id = messaging["sender"]["id"]
     message = hd(messaging)["message"]["text"]
+    user_id = messaging["sender"]["id"]
     IO.inspect(page_access_token)
     IO.inspect(message)
     MessengerAPI.start
-    MessengerAPI.postMessage(%{:user_id => user_id, :message => message})
+    FdlMessengerBot.MessengerAPI.start
+    FdlMessengerBot.MessengerAPI.postMessage(%{:user_id => user_id, :message => message})
     text conn, :ok
   end
 end
